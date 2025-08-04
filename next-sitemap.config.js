cfe4fe5f2a6ce28c220fa08b2e9f058ecff1853a
TMW <backendtmw@gmail.com>
2025-08-05 00:12:42 +0500

    changes of sitemap and robottxt

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://www.gabungpdf.id', // ✅ replace with your actual domain
+  generateRobotsTxt: true, // ✅ generates robots.txt
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateIndexSitemap: true, // useful for large sites
+};
